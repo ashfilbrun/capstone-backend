3ce0ab29515206308477099c3d740a886f1d79c1
ashfilbrun <ashesclaire@gmail.com>
2023-07-28 11:03:46 -0700

    add survey to index

--- models/index.js
+++ models/index.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose')
 const userSchema = require('./user')
 const symptomSchema = require('./symptom')
 const illnessSchema = require('./illness')
+const surveySchema = require('./survey')
 
 const User = mongoose.model('User', userSchema)
 const Symptom = mongoose.model('Symptom', symptomSchema)
 const Illness = mongoose.model('Illness', illnessSchema)
+const Survey = mongoose.model('Survey', surveySchema)
 
 module.exports = {
   User,
   Symptom,
-  Illness
+  Illness,
+  Survey
 }
